3d4a36f2fdb9f53bb0ea4204d180686a0ad41674
Roman Drechsel <email@roman-drechsel.de>
2024-08-01 19:36:40 +0200

    Bugfixes 2024.8.0103

--- src/app/classes/app-meta-data.ts
+++ src/app/classes/app-meta-data.ts
@@ -1,10 +1,13 @@
 import { Device } from "@capacitor/device";
+import { environment } from "../../environments/environment";
 import { AppService } from "../services/app/app.service";
 
 export const AppMetaData = async function(app: AppService) {
     const appInfo = await Device.getInfo();
 
     return {
+        Environment: environment.production ? "Production" : "Development",
+        Release: environment.publicRelease,
         LogMode: app.loggerService.LogLevelShort,
         Resolution: `${app.DeviceWidth}x${app.DeviceHeight}`,
         DeviceIdentifier: AppService.AppInfo.Identifier,
