a661711e767ca74a06a1627704fc20b5c946eef7
Roman Drechsel <email@roman-drechsel.de>
2025-02-11 19:31:13 +0100

    upgraded watch app version to 10

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -11,7 +11,7 @@ export class ConfigService {
     /**
      * build version of the garmin app
      */
-    public readonly GarminAppVersion = 9;
+    public readonly GarminAppVersion = 10;
 
     /**
      * app-id for the Garmin ConnectIQ store
